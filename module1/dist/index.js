444e2ea898b6e69f47da677a3447966038312f3c
aothymoon59 <aothymoon59@gmail.com>
2023-11-03 21:13:10 +0600

    basic data types

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "next level";
+console.log(course);
---
+++ module1/src/1.4.ts
@@ -0,0 +1,25 @@
+// Basic data types
+// string
+let firstName: string = "Moon";
+// number
+let roll: number = 123;
+// boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+// Array
+
+let friends: string[] = ["moon", "soumya"];
+let rollList: number[] = [2, 3];
+
+// tuple --> array --> order --> type of value
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [50, "Mr. X", true];
