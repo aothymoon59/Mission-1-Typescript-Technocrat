bb4076689dc9d82a5f4ac741a03d6c5c82a38ad3
aothymoon59 <aothymoon59@gmail.com>
2023-11-05 20:37:04 +0600

    never,unknown and nullable type

---
+++ module1/src/1.12.ts
@@ -0,0 +1,37 @@
+{
+  //
+  //nullable type
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+
+  searchName(null);
+
+  //   unknown typeof
+  const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else if (typeof value === "string") {
+      const [result, unit] = value.split(" ");
+      const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("wrong input");
+    }
+  };
+  getSpeedInMeterPerSecond(`1000 kmh^-1`);
+
+  //never
+
+  const throwError = (msg: string): never => {
+    throw new Error(msg);
+  };
+
+  throwError("Error 500");
+  //
+}
