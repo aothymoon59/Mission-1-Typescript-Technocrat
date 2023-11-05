976ed0fa6eb282b5d3e62745a693d2d2d4f06502
aothymoon59 <aothymoon59@gmail.com>
2023-11-05 21:40:07 +0600

    module 2 type assertion

---
+++ module2/2.1.ts
@@ -0,0 +1,32 @@
+{
+  //
+  //type assertion
+  let anyThing: any;
+  anyThing = "Next Level Web Development";
+  anyThing = 222;
+  (anyThing as number).toFixed(2);
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The converted value is ${convertedValue}`;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const result1 = kgToGm(1000) as number;
+  const result2 = kgToGm("1000") as number;
+
+  type CustomError = {
+    message: string;
+  };
+
+  try {
+  } catch (error) {
+    console.log((error as CustomError).message);
+  }
+
+  //
+}
