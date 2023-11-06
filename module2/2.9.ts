b94726d791208e1eef5e259b5fcf50e561ae368d
aothymoon59 <aothymoon59@gmail.com>
2023-11-07 04:31:39 +0600

    conditional type

---
+++ module2/2.9.ts
@@ -0,0 +1,21 @@
+{
+  //conditional type
+  type a1 = number;
+  type b1 = string;
+
+  type x = a1 extends null ? true : false; // conditional type
+
+  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  //car ache kina / bike ache kina / ship ache kina / tractor ache kina
+  type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+  type HasTractor = CheckVehicle<"tractor">;
+  //
+}
