a6b9a9e197f14cd13f5180bed6043d19d3703eaa
aothymoon59 <aothymoon59@gmail.com>
2023-11-07 17:49:36 +0600

    type guard using instanceof

---
+++ module3/3.4.ts
@@ -0,0 +1,58 @@
+{
+  // instance of guard
+  class Animal {
+    name: string;
+    species: string;
+    constructor(name: string, species: string) {
+      this.name = name;
+      this.species = species;
+    }
+
+    makeSound() {
+      console.log("I am making sound");
+    }
+  }
+
+  class Dog extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+    makeBark() {
+      console.log("I am barking");
+    }
+  }
+
+  class Cat extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+    makeMeaw() {
+      console.log("I am meawing");
+    }
+  }
+
+  //smart way te handle korar jonno caile amra function babohar korte pari
+
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Cat;
+  };
+
+  const getAnimal = (animal: Animal) => {
+    if (isDog(animal)) {
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
+  };
+
+  const dog = new Dog("Dog vai", "Dog");
+  const cat = new Cat("Cat vai", "Cat");
+
+  getAnimal(cat);
+  //
+}
